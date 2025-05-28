0ee00a6b84f9ba542042de0e6f970f0c1d466d41
kindtiger <seonbeom2@gmail.com>
2025-05-28 15:20:29 +0900

    first commit

---
+++ client/main.js
@@ -0,0 +1,11 @@
+console.log(navigator.language);
+
+/* 여기서부터 변수 선언  */
+
+const a = 10;
+
+/* 여기서부터 함수 선언  */
+
+
+
+
---
+++ server/index.js
@@ -0,0 +1,32 @@
+// console.log(process.env.LANG);
+
+
+
+const server = require('live-server');
+
+const params = {
+  host:'localhost',
+  port:3000,
+  open:false,
+  root:'./client'
+}
+
+
+
+server.start(params)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
