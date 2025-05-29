19aee0f96636dcfe421dec3920744c92af98fe64
kindtiger <seonbeom2@gmail.com>
2025-05-29 15:52:29 +0900

    learn. core javascript
    - codeStructure
    - variables
    - strictMode
    - globalThis
    - legacyVar
    - dataType

---
+++ client/chapter/core/02.variables.js
@@ -0,0 +1,123 @@
+/* ------------------ */
+/* Variables          */
+/* ------------------ */
+
+
+
+let admin;
+let nickName;
+
+
+nickName = 'John';
+admin = nickName;
+
+console.log(admin);
+
+
+const OUR_PLANET_NAME = 'EARTH';
+let currentOnlineUserName = 'TIGER';
+
+currentOnlineUserName = 'BEOM';
+
+
+
+
+
+
+
+
+/* 다음 내용을 분석한 후, 프로그래밍 할 수 있도록 변수와 상수를 작성해봅니다. ----------- */
+
+// - Calculating product prices by number
+
+
+
+// - 상품 정보
+
+/* variables ----------------------------------------------------------- */
+
+
+//  - 갯수 별 상품 가격 계산하기
+let CalcProductPriceQuantity;
+
+
+// - 구매 제품 가격의 총 합
+let totalProductPrice;
+
+// - 구매 결제 여부
+let isPayment = true;
+let hasCartItem = false;
+
+
+// - 구매 결제 내역
+let paymentList;
+
+// - 오늘의 운세
+let fortuneToday = 'gooooood';
+
+
+/* constant variables -------------------------------------------------- */
+
+// - 1년 기준 일(day)자 수 365
+const DAY_PER_YEAR = 365;
+
+
+// - 브랜드 접두사
+
+const BRAND_PREFIX = 'NIKE'
+
+
+
+const product_info = '.....'
+
+const product = {
+  productA :{
+    price:30000,
+    name:'aaa',
+    quantity:3
+  },
+  productB:{
+    price:30000,
+    name:'aaa',
+    quantity:3
+  }
+}
+
+{/* <Button onclick /> */}
+
+
+
+
+const a = {
+  name:'tiger'
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- client/main.js
+++ client/main.js
@@ -1,11 +1,6 @@
-console.log(navigator.language);
-
-/* 여기서부터 변수 선언  */
-
-const a = 10;
-
-/* 여기서부터 함수 선언  */
 
 
+
+console.log(globalThis);
 
 
