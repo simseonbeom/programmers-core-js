88aabceb18e906209b2a5a39ec756ee1d853a933
kindtiger <seonbeom2@gmail.com>
2025-06-24 09:50:20 +0900

    learn. webComponent

---
+++ client/components/Button/Button.js
@@ -0,0 +1,75 @@
+
+
+
+class MyElement extends HTMLElement {
+  
+  count = 0;
+
+  constructor(){
+    super();
+
+  }
+
+  connectedCallback(){
+    console.log('mount');
+    this.render();
+    this.attachEvent();
+  } 
+  
+  disconnectedCallback(){
+    console.log('unmount');
+  }
+
+  static get observedAttributes(){
+
+    return ['data-value']
+  }
+
+  attributeChangedCallback(name, oldValue, newValue){
+    if(name === 'data-value'){
+      this.render();
+    }
+
+    console.log( name, oldValue, newValue );
+  }
+
+
+  handleClick(){
+    console.log('clicked');
+    this.dataset.value = ++this.count;
+    
+  }
+
+
+  attachEvent(){
+    this.addEventListener('click',this.handleClick)
+  }
+
+  render(){
+    console.log();
+    this.innerHTML = ++this.count;
+    
+  }
+  
+
+}
+
+
+// 브라우저에게 알려주는 시점
+customElements.define('my-element',MyElement);
+
+
+
+// const element = document.createElement('my-element');
+// document.body.append(element)
+
+
+
+
+
+
+
+
+
+
+
